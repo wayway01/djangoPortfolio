d759479268bf4f8fdf6d9a075245973ed6ca780b
wayway01 <django.for.akihiro@gmail.com>
2020-08-31 22:50:44 +0900

    modifications to settings and models

--- djangoPortfolio/settings.py
+++ djangoPortfolio/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['wayway.pythonanywhere.com']
+
+ALLOWED_HOSTS = ['localhost','wayway.pythonanywhere.com']
 
 
 # Application definition
